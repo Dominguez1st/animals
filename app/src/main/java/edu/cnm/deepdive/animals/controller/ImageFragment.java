130fbc3efa486107cef0c1a6be07fa02a70a7bc0
Robert Dominguez <dominguez.1st@gmail.com>
2020-10-19 12:11:21 -0600

    Replaced webview w/ imageview
    Also made changes to save data during orientation/configuration change.

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -11,6 +11,7 @@ import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +19,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import com.squareup.picasso.Picasso;
 import edu.cnm.deepdive.animals.R;
 import edu.cnm.deepdive.animals.model.Animal;
 import edu.cnm.deepdive.animals.viewmodel.AnimalViewModel;
@@ -26,18 +28,19 @@ import java.util.Objects;
 
 public class ImageFragment extends Fragment implements OnItemSelectedListener {
 
-  private WebView contentView;
+  private ImageView imageView;
   private AnimalViewModel animalViewModel;
   private Spinner spinner;
   private List<Animal> animals;
   private Toolbar toolbar;
+  private int selectedAnimal = -1;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_image, container, false);
-    setupWebView(root);
     toolbar = root.findViewById(R.id.toolbar);
+    imageView = root.findViewById(R.id.image_view);
     toolbar.setTitle(R.string.app_name);
     spinner = root.findViewById(R.id.animals_spinner);
     spinner.setOnItemSelectedListener(this);
@@ -49,45 +52,50 @@ public class ImageFragment extends Fragment implements OnItemSelectedListener {
   public void onViewCreated(@NonNull View view,
       @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    animalViewModel = new ViewModelProvider(getActivity())
+    animalViewModel = new ViewModelProvider(Objects.requireNonNull(getActivity()))
         .get(AnimalViewModel.class);
     animalViewModel.getAnimals().observe(getViewLifecycleOwner(), new Observer<List<Animal>>() {
       @Override
       public void onChanged(List<Animal> animals) {
         ImageFragment.this.animals = animals;
         ArrayAdapter<Animal> adapter = new ArrayAdapter<>(
-            ImageFragment.this.getContext(), R.layout.custom_spinner_item,animals);
+            ImageFragment.this.getContext(), R.layout.custom_spinner_item, animals);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        if (selectedAnimal >= 0) {
+          updateSelection();
+        }
+
+      }
+    });
+
+    animalViewModel.getSelectedItem().observe(getViewLifecycleOwner(), (item) -> {
+      if (item != selectedAnimal) {
+        selectedAnimal = item;
+        if (animals != null) {
+          updateSelection();
+        }
       }
     });
   }
 
-  private void setupWebView(View root) {
-    contentView = root.findViewById(R.id.content_view);
-    contentView.setWebViewClient(new WebViewClient() {
-      @Override
-      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-        return false;
-      }
-    });
-    WebSettings settings = contentView.getSettings();
-    settings.setJavaScriptEnabled(true);
-    settings.setSupportZoom(true);
-    settings.setBuiltInZoomControls(true);
-    settings.setDisplayZoomControls(false);
-    settings.setUseWideViewPort(true);
-    settings.setLoadWithOverviewMode(true);
+
+  private void updateSelection() {
+    spinner.setSelection(selectedAnimal);
+
+    Picasso.get().load(animals.get(selectedAnimal).getImageUrl())
+        .into(imageView);
+
   }
 
   @Override
-  public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-    contentView.loadUrl(animals.get(position).getImageUrl());
+  public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+    animalViewModel.select(position);
   }
 
   @Override
-  public void onNothingSelected(AdapterView<?> parent) {
+  public void onNothingSelected(AdapterView<?> adapterView) {
 
   }
 }
